ae87e0ae00573db9d4ef977947c24693440fe311
StepanenkoKirill <123152402+StepanenkoKirill@users.noreply.github.com>
2024-10-08 16:42:35 +0300

    trying to make simulataneous logging and stabilisation run

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
 import sys, time
 from PyQt5 import QtWidgets, QtCore, uic
+from GUI.ui import Ui_Form
 
 from Stabilisation.Stabilizatoin import main
 
-class Main(QtWidgets.QWidget):
+class Main(QtWidgets.QWidget, Ui_Form):
     def __init__(self, parent=None):
         super().__init__(parent)
-        uic.loadUi(r'C:\Users\Ольга\PycharmProjects\Qt5Projects\generic_stabilisation_with_gui\GUI\generic_stabilisation_with_gui3.ui', self)
-        self.pushButton_8.clicked.connect(QtWidgets.qApp.quit)
-        self.pushButton_9.clicked.connect(main)
+        self.ui = Ui_Form()
+        self.ui.setupUi(self)
+        # uic.loadUi(r'C:\Users\Ольга\PycharmProjects\Qt5Projects\generic_stabilisation_with_gui\GUI\generic_stabilisation_with_gui3.ui', self)
+        self.ui.pushButton_8.clicked.connect(self.text_edit_filler)
+        self.ui.pushButton_9.clicked.connect(main)
+
         # main() # create another process or make start button
+    def text_edit_filler(self):
+        i = 1
+        while i < 5:
+            self.plainTextEdit.insertPlainText("You can write text here.\n")
+            time.sleep(1)
+            i += 1
+
 
 
 if __name__ == '__main__':
